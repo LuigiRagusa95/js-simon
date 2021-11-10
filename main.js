2ecc038f3f27fed4e8056a82016a2326643747e9
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-10 14:34:56 +0100

    ADD: add reference from HTML

---
+++ main.js
@@ -0,0 +1,4 @@
+const popUpEl = document.getElementById('popup');
+const startBtn = document.getElementById('start-btn');
+const smallText = document.getElementById('small-text');
+const numberText = document.getElementById('number-text');
